0db31c01ba0d627d90d2a7084f7a58cb6f55e14c
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-08-18 13:46:04 +0200

    Changes:
    	- Add Hero section;
    	- Create and add slider;

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,48 @@ import '../scss/Styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-// import './style.css';
+
+// Slider 
+(() => {
+    const sliderLeftButton = document.querySelector(`#left`);
+    const sliderRightButton = document.querySelector(`#right`);
+    const sliderItems = document.querySelectorAll(`.slider-item`);
+
+    let currentSliderActiveElementIndex = 0;
+
+    const clearAllActive = () => [...sliderItems].map(item => item.classList.remove(`slider-item-active`))
+
+    const addActive = () => sliderItems[currentSliderActiveElementIndex].classList.add(`slider-item-active`)
+
+    const handleClickLeft = () => {
+
+        if (currentSliderActiveElementIndex < 1) {
+            currentSliderActiveElementIndex = sliderItems.length - 1
+        } else {
+            currentSliderActiveElementIndex -= 1;
+        }
+
+        clearAllActive()
+        addActive()
+
+    }
+
+
+    const handleClickRight = () => {
+
+        if (currentSliderActiveElementIndex >= sliderItems.length - 1) {
+            currentSliderActiveElementIndex = 0;
+        } else {
+            currentSliderActiveElementIndex += 1;
+        }
+
+        clearAllActive()
+        addActive()
+
+    }
+
+
+    sliderLeftButton.addEventListener(`click`, handleClickLeft)
+    sliderRightButton.addEventListener(`click`, handleClickRight)
+
+})()
